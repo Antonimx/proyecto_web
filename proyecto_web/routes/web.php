7992709675a15eec41b3fc019e40f3ed244e5159
Antonimx <antoniamunozcortes19@gmail.com>
2024-06-20 19:13:55 -0400

    rutas

--- proyecto_web/routes/web.php
+++ proyecto_web/routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\PerfilesController;
 use App\Http\Controllers\TiposController;
 use App\Http\Controllers\VehiculosController;
 
-<<<<<<< HEAD
 //Home
 Route::get('/',[HomeController::class,'index'])->name('home.index')->middleware('auth');
 
@@ -17,15 +16,15 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/usuarios/password',[UsuariosController::class,'password'])->name('usuarios.password');
     Route::get('/usuarios/crear',[UsuariosController::class,'create'])->name('usuarios.create');
     Route::get('/usuarios/logout',[UsuariosController::class,'logout'])->name('usuarios.logout');
-=======
-Route::get('/', function () {
-    return view('home.index');
->>>>>>> origin/main
 });
 Route::get('/usuarios/login',[UsuariosController::class,'login'])->name('usuarios.login');
 Route::post('/usuarios/autenticar',[UsuariosController::class,'autenticar'])->name('usuarios.autenticar');
 
 //Vehiculos
 Route::resource('/vehiculos',VehiculosController::class)->middleware('auth');
+
+//Tipos
 Route::resource('/tipos',TiposController::class)->middleware('auth');
+
+//Perfiles
 Route::resource('/perfiles',PerfilesController::class)->middleware('auth');
