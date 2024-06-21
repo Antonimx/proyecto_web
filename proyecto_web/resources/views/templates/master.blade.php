1ed1c03b2913aab22f2f4614d7a7eabd344e5888
Valen <vnavarro@usm.cl>
2024-06-21 18:28:56 -0400

    home index, usuario index y usuarios create
    aun esqueleto

--- proyecto_web/resources/views/templates/master.blade.php
+++ proyecto_web/resources/views/templates/master.blade.php
@@ -35,7 +35,7 @@
     {{-- / NAVBAR --}}
 
     {{-- CONTENIDO PAGINA --}}
-    <div class="container p-3 bg-light rounded">
+    <div class="container p-2 bg-light rounded">
         @yield('contenido-pagina')
     </div>
     {{-- /CONTENIDO PAGINA --}}
--- proyecto_web/resources/views/usuarios/index.blade.php
+++ proyecto_web/resources/views/usuarios/index.blade.php
@@ -1,3 +1,48 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
+@extends('templates.master')
+
+@section('contenido-pagina')
+
+<div class="row">
+    <div class="col-9 text-body-secondary text-left">
+        <h3>Gestionar usuarios</h3>
+    </div>
+    <a class="col-2 btn btn-primary" href="{{route('usuarios.create')}}" role="button">Crear nuevo usuario</a>
 </div>
+
+
+<hr>
+
+<div class="row">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+    
+
+@endsection
